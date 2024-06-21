eea9c2e4f07fa8479a6bd93e965585897b718d56
emzhran <zachran750@gmail.com>
2024-06-22 02:28:30 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import gambar from "./assets/smddd.jpg";
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+    <h1 className="judul">Samarinda</h1>
+    <img className="gambar" src={gambar} alt="gambar"/>
+    <h1 className="p">Samarinda, ibu kota provinsi Kalimantan Timur, Indonesia, terletak di tepi Sungai Mahakam. Kota ini memainkan peran penting sebagai pusat ekonomi, budaya, dan pemerintahan di wilayah tersebut. Dengan ekonomi yang didominasi oleh sektor perdagangan, jasa, dan industri, Samarinda juga dikenal sebagai pusat industri kayu dan pengolahan hasil hutan. Infrastruktur kota terus berkembang, termasuk jalan, jembatan, dan fasilitas umum lainnya untuk mendukung pertumbuhan kota dan kenyamanan warganya.
+
+    Kota Samarinda adalah tempat yang multikultural, di mana beragam etnis dan budaya hidup berdampingan. Ini tercermin dalam berbagai acara budaya, kuliner khas, serta tempat ibadah yang beragam. Samarinda juga menawarkan berbagai destinasi wisata, mulai dari wisata alam seperti air terjun dan hutan lindung, hingga wisata budaya dan sejarah seperti Museum Samarinda. Sungai Mahakam menjadi daya tarik utama dengan pemandangan indah dan kehidupan sungai yang khas, menjadikan Samarinda sebagai kota yang dinamis dan penuh dengan peluang serta pengalaman budaya yang kaya.</h1>
+    </>
+  )
+}
+
+export default App
